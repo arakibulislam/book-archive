b460c1ed84232dad0b56a767808561546c071b38
maisha haider <arakibulislam>
2021-09-02 15:00:13 +0600

    akash

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const inputText = document.getElementById('input');
 const displayBook = document.getElementById('show-book');
 const bookFound = document.getElementById('book-found');
 const spinner = document.getElementById('spinner');
-const alart = document.getElementById('alart')
-const alart1 = document.getElementById('alart-1')
+const alert = document.getElementById('alert')
+const alert1 = document.getElementById('alert-1')
 
 // onclick function
 const searchBook = () => {
     if (inputText.value === '') {
-        alart.style.display = 'block'
-        alart1.style.display = 'none'
+        alert.style.display = 'block'
+        alert1.style.display = 'none'
         bookFound.innerHTML = '';
         displayBook.innerHTML = '';
     }
     else {
         getBook(inputText.value);
         spinner.style.display = 'block'
-        alart.style.display = 'none'
-        alart1.style.display = 'none'
+        alert.style.display = 'none'
+        alert1.style.display = 'none'
         bookFound.innerHTML = '';
         displayBook.innerHTML = '';
     }
@@ -35,7 +35,7 @@ const showBook = books => {
     // condition
     const bookDetails = books.docs;
     if (bookDetails.length === 0) {
-        alart1.style.display = 'block'
+        alert1.style.display = 'block'
         spinner.style.display = 'none';
         inputText.value = '';
     }
@@ -51,21 +51,22 @@ const showBook = books => {
             const div = document.createElement("div");
             div.classList.add("col");
             div.innerHTML = `
-        <div class="card mb-3 h-100" style="max-width: 540px;">
+                <div class="card mb-3 h-100  border-0 rounded-3 shadow books mx-2">
                     <div class="row g-0">
                         <div class="col-md-4">
                             <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : '10909258'}-M.jpg" class="img-fluid rounded-start" alt="...">
                         </div>
-                        <div class="col-md-8">
+                        <div class="col-md-8 ">
                             <div class="card-body">
-                                <h5 class="card-title">${book.title} </h5>
-                                <p class="card-text">Author : ${book.author_name ? book.author_name : 'not found'}</p>
-                                <p class="card-text">Publisher : ${book.publisher ? book.publisher : 'not found'}</p>
-                                <p class="card-text">First Publish Year : ${book.first_publish_year ? book.first_publish_year : 'not found'}</p>
+                                <h5 class="card-title fs-4 ">${book.title} </h5>
+                                <p class="card-text"><span class="fw-bold ">Author :</span> ${book.author_name ? book.author_name : 'not found'}</p>
+                                <p class="card-text"><span class="fw-bold ">Publisher :</span> ${book.publisher ? book.publisher : 'not found'}</p>
+                                <p class="card-text"><span class="fw-bold ">First publish year :</span> ${book.first_publish_year ? book.first_publish_year : 'not found'}</p>
                             </div>
-                        </div>
-                    </div>
+                        </div>                       
+                    </div>                   
                 </div>
+                
         `;
             displayBook.appendChild(div);
             spinner.style.display = 'none';
